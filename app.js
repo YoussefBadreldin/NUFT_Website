baf00ba7ed7b47180808d391a510f218e4449182
Youssef Badreldin <youssefmbadreldin@gmail.com>
2024-06-16 17:39:33 +0300

    new features

--- app.js
+++ app.js
@@ -318,10 +318,10 @@ app.get('/CommingSoon', (req, res) => {
 });
 
 
-// Start server
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http:localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 module.exports = app;

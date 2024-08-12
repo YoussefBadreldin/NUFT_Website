f4e42642c0846afb37a21960ddf996ed69b578a9
Youssef Badreldin <youssefmbadreldin@gmail.com>
2024-08-12 13:48:52 +0300

    Fix build issues

--- src/views/Universites/Special/Special.vue
+++ src/views/Universites/Special/Special.vue
@@ -3,43 +3,43 @@
     <HeaderComponent />
  
     <div class="page-nav row">
-        <h2>دليل الجامعات الاهلية</h2>
+        <h2>دليل الجامعات ذات طبيعة خاصة</h2>
 
     </div>
 
     <div class="Card-Box" dir="rtl">
         <div class="container" style="text-align: center;">
-        <h3>ما هي الجامعات الاهلية المصرية؟</h3>
-        هي جامعات غير ربحية تعيد استثمار جميع الرسوم في ميزانية المؤسسة لتعزيز مرافق الجامعة وخدماتها. تشتهر برسومها المنخفضة مقارنةً بالجامعات الخاصة وبالمناهج التعليمية المتقدمة والقدرات التي توفرها بالتعاون مع كيانات محلية وأجنبية. تخضع لقانون الجامعات الخاصة والاهلية رقم 12 لسنة 2009، ولائحته التنفيذية، والقانون رقم 143 لسنة 2019، الذي يعدل بعض أحكام قانون الجامعات الخاصة الاهلية الصادر بالقانون رقم 12 لسنة 2009. وتحت إشراف المجلس الأعلى للجامعات الاهلية. وهم 20 جامعة  .
+        <h3>ما هي الجامعات ذات طبيعة خاصة؟</h3>
+        هي جامعات . وهم 10 جامعة
         <br><br>
-        <h3><br>أنواع الجامعات الاهلية</h3>
+        <h3><br>أنواع الجامعات ذات طبيعة خلصة</h3>
         <div class="row Box-ro">
             <div class="col-md-4">
-            <router-link to="/Guide/UGRAD/National/Normal">
+            <router-link to="/Guide/UGRAD/special/InternationalAgreements">
                 <div class="Box-card">
-                <img src="/images/Categories/NormalNU.png" alt="Normal National Universities">
+                <img src="/images/Categories/InternationalAgreements.png" alt=" special Universities">
                 <div class="Box-det" style="text-align: center;">
-                    <h6>الجامعات الاهلية</h6>
+                    <h6>جامعات باتفاقات دولية</h6>
                 </div>
                 </div>
             </router-link>
             </div>
             <div class="col-md-4">
-            <router-link to="/Guide/UGRAD/National/International">
+            <router-link to="/Guide/UGRAD/special/FrameworkAgreements">
                 <div class="Box-card">
-                <img src="/images/Categories/InternationalNU.png" alt="International National Universities">
+                <img src="/images/Categories/FrameworkAgreements.png" alt=" special Universities">
                 <div class="Box-det" style="text-align: center;">
-                    <h6>الجامعات الاهلية الدولية</h6>
+                    <h6>جامعات باتفاقات اطارية</h6>
                 </div>
                 </div>
             </router-link>
             </div>
             <div class="col-md-4">
-            <router-link to="/guide/UGRAD/National/Affiliated">
+            <router-link to="/guide/UGRAD/special/SpecialLaw">
                 <div class="Box-card">
-                <img src="/images/Categories/AffiliatedNU.png" alt="Affiliated National Universities">
+                <img src="/images/Categories/SpecialLaw.png" alt=" special Universities">
                 <div class="Box-det" style="text-align: center;">
-                    <h6>الجامعات الاهلية المنبثقة</h6>
+                    <h6>جامعات بقوانين خاصة</h6>
                 </div>
                 </div>
             </router-link>

61d8d8468d28c2effdd95716e403b80b586a4f3f
Youssef Badreldin <youssefmbadreldin@gmail.com>
2024-08-21 00:33:04 +0300

    Fix build issues

--- src/views/Universites/Private/Private.vue
+++ src/views/Universites/Private/Private.vue
@@ -178,7 +178,7 @@ export default {
         },
         {
             id: 22,
-            name: 'جامعة ميرت',
+            name: 'جامعة ميريت',
             imgSrc: '/images/Logos/Universites/Private/MUE.png',
             link: '/guide/UGRAD/private/MUE'
         },

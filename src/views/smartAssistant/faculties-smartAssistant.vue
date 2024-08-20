cbdb200586bffb96dc6c42bdebb0e08c30fc22b9
Youssef Badreldin <youssefmbadreldin@gmail.com>
2024-08-20 23:32:38 +0300

    Fix build issues

--- src/views/smartAssistant/faculties-smartAssistant.vue
+++ src/views/smartAssistant/faculties-smartAssistant.vue
@@ -72,88 +72,90 @@ export default {
 },
 {
     id: 5,
-    name: 'كلية الهندسة',
-    imgSrc: '/images/Logos/Faculties/ENG.png',
-    link: '/SmartAssistant/Faculties/ENG'
-},
-{
-    id: 6,
     name: 'كلية الطب البيطري',
     imgSrc: '/images/Logos/Faculties/VET.png',
     link: '/SmartAssistant/Faculties/VET'
 },
+
 {
-    id: 7,
-    name: 'كلية علوم الحاسب',
-    imgSrc: '/images/Logos/Faculties/CS.png',
-    link: '/SmartAssistant/Faculties/CS'
-},
-{
-    id: 8,
+    id: 6,
     name: 'كلية التمريض',
     imgSrc: '/images/Logos/Faculties/NURS.png',
     link: '/SmartAssistant/Faculties/NURS'
 },
 {
-    id: 9,
+    id: 7,
     name: 'كلية تكنولوجيا العلوم الصحية',
     imgSrc: '/images/Logos/Faculties/HS.png',
     link: '/SmartAssistant/Faculties/HS'
 },
 {
-    id: 10,
-    name: 'كلية الفنون',
-    imgSrc: '/images/Logos/Faculties/ART.png',
-    link: '/SmartAssistant/Faculties/ART'
-},
-{
-    id: 11,
-    name: 'كلية الإعلام',
-    imgSrc: '/images/Logos/Faculties/MEDIA.png',
-    link: '/SmartAssistant/Faculties/MEDIA'
-},
-{
-    id: 12,
-    name: 'كلية الآثار',
-    imgSrc: '/images/Logos/Faculties/ARCH.png',
-    link: '/SmartAssistant/Faculties/ARCH'
-},
-{
-    id: 13,
-    name: 'كلية الحقوق',
-    imgSrc: '/images/Logos/Faculties/LAW.png',
-    link: '/SmartAssistant/Faculties/LAW'
-},
-{
-    id: 14,
-    name: 'كلية السياحة',
-    imgSrc: '/images/Logos/Faculties/TOUR.png',
-    link: '/SmartAssistant/Faculties/TOUR'
-},
-{
-    id: 15,
-    name: 'كلية الاقتصاد والإدارة',
-    imgSrc: '/images/Logos/Faculties/ECON.png',
-    link: '/SmartAssistant/Faculties/ECON'
-},
-{
-    id: 16,
-    name: 'كلية العلوم السينمائية',
-    imgSrc: '/images/Logos/Faculties/CINE.png',
-    link: '/SmartAssistant/Faculties/CINE'
-},
-{
-    id: 17,
+    id: 8,
     name: 'كلية العلوم الأساسية',
     imgSrc: '/images/Logos/Faculties/SCI.png',
     link: '/SmartAssistant/Faculties/SCI'
 },
 {
-    id: 18,
+    id: 9,
     name: 'كلية التكنولوجيا الحيوية',
     imgSrc: '/images/Logos/Faculties/BIOTECH.png',
     link: '/SmartAssistant/Faculties/BIOTECH'
 },
+{
+    id: 10,
+    name: 'كلية الهندسة',
+    imgSrc: '/images/Logos/Faculties/ENG.png',
+    link: '/SmartAssistant/Faculties/ENG'
+},
+{
+    id: 11,
+    name: 'كلية علوم الحاسب',
+    imgSrc: '/images/Logos/Faculties/CS.png',
+    link: '/SmartAssistant/Faculties/CS'
+},
+{
+    id: 12,
+    name: 'كلية الفنون',
+    imgSrc: '/images/Logos/Faculties/ART.png',
+    link: '/SmartAssistant/Faculties/ART'
+},
+{
+    id: 13,
+    name: 'كلية الإعلام',
+    imgSrc: '/images/Logos/Faculties/MEDIA.png',
+    link: '/SmartAssistant/Faculties/MEDIA'
+},
+{
+    id: 14,
+    name: 'كلية الآثار',
+    imgSrc: '/images/Logos/Faculties/ARCH.png',
+    link: '/SmartAssistant/Faculties/ARCH'
+},
+{
+    id: 15,
+    name: 'كلية الحقوق',
+    imgSrc: '/images/Logos/Faculties/LAW.png',
+    link: '/SmartAssistant/Faculties/LAW'
+},
+{
+    id: 16,
+    name: 'كلية السياحة',
+    imgSrc: '/images/Logos/Faculties/TOUR.png',
+    link: '/SmartAssistant/Faculties/TOUR'
+},
+{
+    id: 17,
+    name: 'كلية الاقتصاد والإدارة',
+    imgSrc: '/images/Logos/Faculties/ECON.png',
+    link: '/SmartAssistant/Faculties/ECON'
+},
+{
+    id: 18,
+    name: 'كلية العلوم السينمائية',
+    imgSrc: '/images/Logos/Faculties/CINE.png',
+    link: '/SmartAssistant/Faculties/CINE'
+},
+
 {
     id: 19,
     name: 'كلية التربية',

dab39ca9e7ba6c0f5a9ece86473f9a8413c21177
Youssef Badreldin <youssefmbadreldin@gmail.com>
2024-08-20 14:08:31 +0300

    Fix build issues

--- src/router/index.js
+++ src/router/index.js
@@ -114,6 +114,27 @@ import searchsmartAssistant from "../views/smartAssistant/search-smartAssistant.
 import scoresmartAssistant from "../views/smartAssistant/score-smartAssistant.vue";
 import facultiessmartAssistant from "../views/smartAssistant/faculties-smartAssistant.vue";
 import MED from "../views/smartAssistant/MED.vue";
+import DENT from "../views/smartAssistant/DENT.vue";
+import PT from "../views/smartAssistant/PT.vue";
+import PHARM from "../views/smartAssistant/PHARM.vue";
+import ENG from "../views/smartAssistant/ENG.vue";
+import VET from "../views/smartAssistant/VET.vue";
+import CS from "../views/smartAssistant/CS.vue";
+import NURS from "../views/smartAssistant/NURS.vue";
+import HS from "../views/smartAssistant/HS.vue";
+import ART from "../views/smartAssistant/ART.vue";
+import MEDIA from "../views/smartAssistant/MEDIA.vue";
+import ARCH from "../views/smartAssistant/ARCH.vue";
+import LAW from "../views/smartAssistant/LAW.vue";
+import TOUR from "../views/smartAssistant/TOUR.vue";
+import ECON from "../views/smartAssistant/ECON.vue";
+import CINE from "../views/smartAssistant/CINE.vue";
+import SCI from "../views/smartAssistant/SCI.vue";
+import BIOTECH from "../views/smartAssistant/BIOTECH.vue";
+import EDU from "../views/smartAssistant/EDU.vue";
+import LANG from "../views/smartAssistant/LANG.vue";
+import SOCSCI from "../views/smartAssistant/SOCSCI.vue";
+
 const routes = [
   {
     path:'/',
@@ -580,6 +601,106 @@ const routes = [
     name:"MED",
     component:MED
   },
+  {
+    path: "/smartAssistant/faculties/DENT",
+    name: "DENT",
+    component: DENT
+  },
+  {
+    path: "/smartAssistant/faculties/PT",
+    name: "PT",
+    component: PT
+  },
+  {
+    path: "/smartAssistant/faculties/PHARM",
+    name: "PHARM",
+    component: PHARM
+  },
+  {
+    path: "/smartAssistant/faculties/ENG",
+    name: "ENG",
+    component: ENG
+  },
+  {
+    path: "/smartAssistant/faculties/VET",
+    name: "VET",
+    component: VET
+  },
+  {
+    path: "/smartAssistant/faculties/CS",
+    name: "CS",
+    component: CS
+  },
+  {
+    path: "/smartAssistant/faculties/NURS",
+    name: "NURS",
+    component: NURS
+  },
+  {
+    path: "/smartAssistant/faculties/HS",
+    name: "HS",
+    component: HS
+  },
+  {
+    path: "/smartAssistant/faculties/ART",
+    name: "ART",
+    component: ART
+  },
+  {
+    path: "/smartAssistant/faculties/MEDIA",
+    name: "MEDIA",
+    component: MEDIA
+  },
+  {
+    path: "/smartAssistant/faculties/ARCH",
+    name: "ARCH",
+    component: ARCH
+  },
+  {
+    path: "/smartAssistant/faculties/LAW",
+    name: "LAW",
+    component: LAW
+  },
+  {
+    path: "/smartAssistant/faculties/TOUR",
+    name: "TOUR",
+    component: TOUR
+  },
+  {
+    path: "/smartAssistant/faculties/ECON",
+    name: "ECON",
+    component: ECON
+  },
+  {
+    path: "/smartAssistant/faculties/CINE",
+    name: "CINE",
+    component: CINE
+  },
+  {
+    path: "/smartAssistant/faculties/SCI",
+    name: "SCI",
+    component: SCI
+  },
+  {
+    path: "/smartAssistant/faculties/BIOTECH",
+    name: "BIOTECH",
+    component: BIOTECH
+  },
+  {
+    path: "/smartAssistant/faculties/EDU",
+    name: "EDU",
+    component: EDU
+  },
+  {
+    path: "/smartAssistant/faculties/LANG",
+    name: "LANG",
+    component: LANG
+  },
+  {
+    path: "/smartAssistant/faculties/SOCSCI",
+    name: "SOCSCI",
+    component: SOCSCI
+  },  
   {
     path:'/Guide/UGRAD/special',
     name:'special',
